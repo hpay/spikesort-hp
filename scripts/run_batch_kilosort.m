f5630e895a92263015766523b4345966ba29f531
Hannah Payne <hpay42@gmail.com>
2022-11-05 00:08:05 -0400

    Reorganization to batch process

--- scripts/run_batch_kilosort.m
+++ scripts/run_batch_kilosort.m
@@ -1,12 +1,18 @@
 %% Generate list of directories to run kilosort on
-birdID = 'HC05'; % AMB111
+% birdID = 'HC05'; % AMB111
 T = readtable('D:\hannah\Dropbox\alab\Analysis\RECORDING_DEPTH_CHICK.xlsx');
 T = T(~T.exclude,:);
-T = T(strcmp(T.bird, birdID),:)
+% T = T(strcmp(T.bird, birdID),:)
 fs = 3e4;
 
+% Overwrite kilosort output?
+overwrite = 1;
+
 % Data folder
-dataDir = 'Z:\Hannah\Ephys\Project2';
+data_dir = 'Z:\Hannah\Ephys\Project2';
+
+% Save dir
+save_dir_fun = @(root_dir) fullfile(root_dir,'kilosort2_output');
 
 %% set up code folders
 scripts_dir = cd;
@@ -20,59 +26,73 @@ addpath(genpath(fullfile(code_dir, 'kilosort-2.0')))
 
 %% Run a single session using settings
 %{
+raw_dir = 'Z:\Hannah\ephys\project2\HC05_220819\raw_220819_125309'
 ops = [];
-% ops.chanMap = 'H6_shankA.mat';
-ops.chanMap = 'H6.mat';
+ops.chanMap = 'H6_shankA.mat';
 ops = hp_config(ops);
 
-% rootDir = 'Z:\Hannah\ephys\project2\HC05_220819\raw_220819_125309'
-rootDir = 'Z:\Hannah\ephys\project2\test64b\raw'
-saveDir = fullfile(rootDir,'kilo2.0'); mkdir(saveDir)
-fprintf('\n Running Kilosort on directory %s \n', rootDir)
-t = tic
-run_single_kilosort(rootDir,saveDir,ops)
+save_dir = save_dir_fun(fileparts(raw_dir)); mkdir(save_dir)
+fprintf('\n Running Kilosort on directory %s \n', root_dir)
+t = tic;
+run_single_kilosort(root_dir,save_dir,ops)
 toc(t)
+
 % Save waveforms for celltype clustering
-wvStruct = getSessionWaveforms(rootDir, saveDir, 0);
-save(fullfile(saveDir, 'waveformStruct.mat'), 'wvStruct')
+wvStruct = getSessionWaveforms(root_dir, save_dir, 0);
+save(fullfile(save_dir, 'waveformStruct.mat'), 'wvStruct')
 
 % Load spikes into my dat structure in matlab
 option_only_good= 1;
-S = importKilo2(saveDir, ops.fs, option_only_good)
+S = importKilo2(save_dir, ops.fs, option_only_good);
+disp(S)
 
 % Run phy to inspect it! (TODO)
 % In Anaconda prompt:
-% conda activate phy_env
+%   conda activate phy_env
 % Navigate to folder with kilosort results (Type Z: to change to engram drive. then cd path. )
-% phy template-gui params.py
+%   phy template-gui params.py
 %}
 
 %% Run all sessions using settings in ops
 for ii = 1:height(T)
-
-    % Find the binary directory
-    rootDir_temp = dir(fullfile(dataDir, T.filename{ii}, 'raw*'));
-    rootDir = fullfile(rootDir_temp.folder, rootDir_temp.name);
+    try
+        % Find the binary directory
+        root_dir = fullfile(data_dir, T.filename{ii});
+        temp = dir(fullfile(data_dir, T.filename{ii}, 'raw*'));
+        raw_dir = fullfile(temp.folder, temp.name);
         
-    % Create save directory
-    saveDir = fullfile(rootDir,'kilo2.0');  mkdir(saveDir);
-         
-    % set up options
-    ops = [];
-    ops.badChannels = [];
-    ops.chanMap = [T.probe_chanmap{ii} '.mat']; % Make sure config folder is on the search path
-    ops = hp_config(ops);    
-    
-    % Run and save results
-    fprintf('\n Running Kilosort on directory %s \n', rootDir),
-    run_single_kilosort(rootDir,saveDir, ops)
-    
-    % Save current options
-    save(fullfile(saveDir,'ops.mat'),'ops')
-    
-    % Save waveforms for celltype clustering
-    wvStruct = getSessionWaveforms(rootDir, saveDir, 0);
-    save(fullfile(saveDir, 'waveformStruct.mat'), 'wvStruct')
+        % Create save directory
+        save_dir = save_dir_fun(root_dir);  mkdir(save_dir);
+        
+        % If it exists already, either overwrite or skip
+        if exist(save_dir,'dir')
+            if overwrite
+                rmdir(save_dir)
+            else
+                continue;
+            end
+        end
+        mkdir(save_dir)
+        
+        % set up options
+        ops = [];
+        ops.badChannels = [];
+        ops.chanMap = [T.probe_chanmap{ii} '.mat']; % Make sure config folder is on the search path
+        ops = hp_config(ops);
+        
+        % Run and save results
+        fprintf('\n Running Kilosort on directory %s \n', raw_dir),
+        run_single_kilosort(raw_dir, save_dir, ops)
+        
+        % Save current options
+        save(fullfile(save_dir,'ops.mat'),'ops')
+        
+        % Save waveforms for celltype clustering
+        wvStruct = getSessionWaveforms(raw_dir, save_dir, 0);
+        save(fullfile(save_dir, 'waveformStruct.mat'), 'wvStruct')
+    catch msg
+        disp(msg)
+    end
 end
 
 
@@ -80,21 +100,16 @@ end
 clear S
 for ii = 1:height(T)
 
-
-    % Find the binary directory
-    rootDir_temp = dir(fullfile(dataDir, T.filename{ii}, 'raw*'));
-    rootDir = fullfile(rootDir_temp.folder, rootDir_temp.name);
-        
-    % Create save directory
-    saveDir = fullfile(rootDir,'kilo2.0');  
-    
+    % Find the save directory
+    save_dir = save_dir_fun(fullfile(data_dir, T.filename{ii}));      
     option_only_good = 1;
-    S{ii} =  importKilo2(saveDir, fs, option_only_good)
+    S{ii} =  importKilo2(save_dir, fs, option_only_good);
         
 end
-ncells = cellfun(@length,S)
-days_since_surgery = days(T.date-datetime(2022,08,09))
-% figure;
+ncells = cellfun(@length,S);
+days_since_surgery = days(T.date-datetime(2022,08,09));
+
+figure;
 hold on
 plot(days_since_surgery, ncells,'-ob')
 xlabel('Days since surgery'); ylabel('# "good" cells in Kilosort2 (SC params)')
