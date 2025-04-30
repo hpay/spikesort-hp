75d308f6376bdcfea4e47a8a73a3fa47821e99bb
Hannah Payne <hpay42@gmail.com>
2025-04-29 22:44:09 -0400

    Added my local version of kilosort2.0 to the repo
    To create a frozen version of spike sorting used in Payne & Aronov 2025

---
+++ kilosort-2.0/utils/get_phy_cluster_labels.m
@@ -0,0 +1,12 @@
+function [unit_ID,cluster_labels] = get_phy_cluster_labels(ksDir)
+% reads cluster_group.tsv from kilosort/phy, and gets cluster labels
+
+fid = fopen(fullfile(ksDir,'cluster_group.tsv'));
+C = textscan(fid, '%s%s');
+fclose(fid);
+
+unit_ID = cellfun(@str2num, C{1}(2:end), 'uni', false);
+ise = cellfun(@isempty, unit_ID);
+unit_ID = [unit_ID{~ise}];
+unit_ID = unit_ID(:);
+cluster_labels = C{2}(2:end);
